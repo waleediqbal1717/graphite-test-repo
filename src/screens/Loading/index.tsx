c873ae06922d1a48919d8582eaddeddab0d1979d
waleediqbal1717 <waleediqbal1717@gmail.com>
2024-02-29 21:55:04 +0500

    its practicce change

--- src/screens/Loading/index.tsx
+++ src/screens/Loading/index.tsx
@@ -1,15 +1,15 @@
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-import * as React from 'react';
-import { useEffect, useRef } from 'react';
-import { Animated, StatusBar, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
-import type { RootStackParamList } from '../../../App';
-import styles from './styles';
-import theme from '../../theme';
-import { AppState } from '../../redux/reducers';
+import * as React from "react";
+import { useEffect, useRef } from "react";
+import { Animated, StatusBar, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import type { RootStackParamList } from "../../../App";
+import styles from "./styles";
+import theme from "../../theme";
+import { AppState } from "../../redux/reducers";
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Loading'>;
+type Props = NativeStackScreenProps<RootStackParamList, "Loading">;
 
 function Loading({ navigation }: Props) {
   const generalSelector = (state: AppState) => state.generalReducer;
@@ -28,9 +28,9 @@ function Loading({ navigation }: Props) {
     fadeIn();
     setTimeout(() => {
       if (isOnBoardingCompleted) {
-        navigation.navigate('Login');
+        navigation.navigate("Login");
       } else {
-        navigation.navigate('OnBoarding');
+        navigation.navigate("OnBoarding");
       }
     }, 2000);
   }, []);
@@ -40,16 +40,6 @@ function Loading({ navigation }: Props) {
         barStyle="dark-content"
         backgroundColor={theme.colors.primary}
       />
-      <Animated.View
-        style={{
-          ...styles.container,
-          opacity: fadeAnim,
-        }}
-      >
-        <Text style={styles.headerText}>{selectedLang.WEL_1}</Text>
-        <View style={styles.divider} />
-        <Text style={styles.subtitle}>{selectedLang.WEL_2}</Text>
-      </Animated.View>
     </View>
   );
 }
